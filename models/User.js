b9369cdfcbe075256f326eb47f70b889f658c2dc
khw0512 <hwunderstand@gmail.com>
2022-09-25 17:05:37 +0900

    토큰까지

--- models/User.js
+++ models/User.js
@@ -1,36 +1,91 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
-    name:{
+    name: {
         type: String,
         maxlength: 50
     },
-    email:{
+    email: {
         type: String,
         trim: true,
         unique: 1
     },
-    password:{
+    password: {
         type: String,
-        minlength:5
+        minlength: 5
     },
-    lastname:{
+    lastname: {
         type: String,
-        maxlength:50
+        maxlength: 50
     },
-    role:{
+    role: {
         type: Number,
-        default:0
+        default: 0
     },
     image: String,
-    token:{
-        type: String,
+    token: {
+        type: String
     },
-    tokenExp:{
-        type:Number
+    tokenExp: {
+        type: Number
     }
 })
 
-const User = mongoose.model('User', userSchema)
 
-module.exports = {User}
+userSchema.pre('save', function (next) {
+
+    var user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+    
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+                // Store hash in your password DB.
+            })
+        })
+    }
+    //비밀번호 암호화
+    else {
+        next()
+    }
+
+
+})
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+
+    //plainPassword 1234567    암호회된 비밀번호 $2b$10$l492vQ0M4s9YUBfwYkkaZOgWHExahjWC
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    //jasonwebtoken 이용
+    var token = jwt.sign(user._id.toHexString(),'secretToken');
+
+    //user._id + 'secretToken' = token
+    //->
+    //'secretToken' =>user._id
+
+    user.token = token
+    user.save(function(err,user){
+        if(err) return cb(err);
+        cb(null, user)
+    })
+}
+
+
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {User};
