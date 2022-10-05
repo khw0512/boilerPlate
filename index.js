7493e49bfa44a3cfe798f453cba43204607c25b6
khw0512 <hwunderstand@gmail.com>
2022-10-05 22:37:54 +0900

    1st commit

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const port = 3000;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
-
-const {User} = require("./models/User")
+const {auth} = require('./middleware/auth');
+const {User} = require("./models/User");
 
 //application/x-www-form-urlenccoded
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,8 +13,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(cookieParser());
-
-
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
@@ -24,7 +22,7 @@ mongoose.connect(config.mongoURI,{
 
 app.get('/', (req,res) => res.send('Hello World!! ~ I am Jason!! How are you?'))
 
-app.post('/register', (req,res) => {
+app.post('/api/users/register', (req,res) => {
     
     //회원 가입할 때 필요한 정보들을 클라이언트에서 가져오면 그것들을 데이터베이스에 넣어준다
     const user = new User(req.body)
@@ -38,7 +36,7 @@ app.post('/register', (req,res) => {
 
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
     // console.log('ping')
     //요청된 이메일을 데이터베이스에서 있는지 찾는다.
@@ -73,7 +71,33 @@ app.post('/login', (req, res) => {
       })
     })
   })
+
+//role 1 admin , role 2 특정 부서 admin
+//role 0 -> 일반유저, role 0이 아니면 관리자
+
+app.get('/api/users/auth', auth, (req,res) =>{
+  //여기까지  미들웨어를 통과해 왔다는 이야기는 authentication 이 True 라는 말
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth:true,
+    email:req.user.email,
+    name:req.user.name,
+    lastname:req.user.lastname,
+    role:req.user.role,
+    image:req.user.image
+  })
+})  
   
+app.get('/api/users/logout', auth, (req,res)=>{
+  User.findOneAndUpdate({_id:req.user._id,},
+    {token:""},
+    (err,user)=>{
+      if(err) return res.json({success:false,err})
+      return res.status(200).send({
+        success:true
+      })
+    })
+})
 
-
-app.listen(port, () => console.log('Example app listening on port ${port}!!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -68,6 +68,7 @@ userSchema.methods.comparePassword = function (plainPassword, cb) {
     })
 }
 
+//methods 는 this =  userSchema
 userSchema.methods.generateToken = function(cb){
     var user = this;
     //jasonwebtoken 이용
@@ -84,7 +85,20 @@ userSchema.methods.generateToken = function(cb){
     })
 }
 
+//statics 는 this = mongoose 모델
+userSchema.statics.findByToken = function (token, cb){
+    var user =  this;
 
+    //user._id + '' = token
+    //토큰을 디코드함
+    jwt.verify(token, 'secretToken', function(err,decoded){
+        //유저 아이디를 이용해서 유저를 찾은 후 클라이언트에서 가져온 토큰과 DB에 보관된 토큰이 일치하는지 확인
+        user.findOne({"_id": decoded, "token":token}, function(err,user){
+            if(err) return cb(err);
+            cb(null, user)
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema);
 
